e03e29e232fd446a225bb26c903878dfafbbc53d
Vitaliy Melnychuk <rozkish.fat@gmail.com>
2021-03-29 23:07:26 +0300

    added edits

--- js/menu.js
+++ js/menu.js
@@ -2,17 +2,14 @@
   const refs = {
     openMenuBtn: document.querySelector('[data-menu-open]'),
     menu: document.querySelector('[data-menu]'),
-  };
-  const icon = {
-    closeIcon: document.querySelector('[close-icon]'),
     menuIcon: document.querySelector('[data-icon]'),
   };
-
+  
   refs.openMenuBtn.addEventListener('click', toggleMenu);
-  icon.closeIcon.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-open');
-    icon.menuIcon.classList.toggle('is-open');
+    refs.menuIcon.classList.toggle('is-open');
+    refs.openMenuBtn.classList.toggle('is-open');
   }
 })();

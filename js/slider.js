6e39f1cf904df02588250de1bdc1cf0e9494b5ee
Vitaliy Melnychuk <rozkish.fat@gmail.com>
2021-04-06 00:20:23 +0300

    уа

--- js/slider.js
+++ js/slider.js
@@ -1,10 +1,54 @@
 var masPreview = document.getElementsByClassName('preview__item');
 var masReviews = document.getElementsByClassName('reviews__item');
+var zz = 5;
 function previews(zm) {
-  for ( i = 0; i < masReviews.length; i++) {
-    masReviews[i].style.opacity = "0";
-    masReviews[i].style.position = "absolute";
+  if (zm < 7 ) {
+    for ( i = 0; i < masReviews.length; i++) {
+      masReviews[i].style.opacity = "0";
+      masReviews[i].style.position = "absolute";
+    }
+    masReviews[zm].style.opacity = "1";
+    masReviews[zm].style.position = "relative";
+    zz = zm;
   }
-  masReviews[zm].style.opacity = "1";
-  masReviews[zm].style.position = "relative";
+  if (zm === 8) {
+    for ( i = 0; i < masReviews.length; i++) {
+      masReviews[i].style.opacity = "0";
+      masReviews[i].style.position = "absolute";
+    }
+    zz = zz - 1;
+    if (zz > 0) {
+      masReviews[zz].style.opacity = "1";
+      masReviews[zz].style.position = "relative";
+    }
+    if (zz === 0) {
+      masReviews[zz].style.opacity = "1";
+      masReviews[zz].style.position = "relative";
+    }
+    if (zz < 0) {
+      zz = 6;
+      masReviews[zz].style.opacity = "1";
+      masReviews[zz].style.position = "relative";
+    }
+  }
+  if (zm === 9) {
+    for ( i = 0; i < masReviews.length; i++) {
+      masReviews[i].style.opacity = "0";
+      masReviews[i].style.position = "absolute";
+    }
+    zz = zz + 1;
+    if (zz < 6) {
+      masReviews[zz].style.opacity = "1";
+      masReviews[zz].style.position = "relative";
+    }
+    if (zz === 6) {
+      masReviews[zz].style.opacity = "1";
+      masReviews[zz].style.position = "relative";
+    }
+    if (zz > 6) {
+      zz = 0;
+      masReviews[zz].style.opacity = "1";
+      masReviews[zz].style.position = "relative";
+    }
+  }
 }
